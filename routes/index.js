74c19bf43d80e1c4e4f6005786a4b9fc93241938
Sreehari <sreeharims2000@gmail.com>
2021-04-24 20:59:21 +0530

    home, scoreboard updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 module.exports = function(app) {
-    app.get('/', function(req, res) {
-        res.render('pages/index');
+    
+    app.get('/home', function(req, res) {
+        res.render('pages/home');
+    });
+    
+    app.get('/', function (req, res) {
+        res.redirect('/home');
     });
 
     app.get('/events', function(req, res) {
@@ -14,5 +19,7 @@ module.exports = function(app) {
     app.get('/schedule', function(req, res) {
         res.render('pages/schedule');
     });
+
+    
     
 };
